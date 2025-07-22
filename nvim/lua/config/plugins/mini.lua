975d8267821c643b44b19783b1fbc7d6a978a684
viodid <david.yunta.aller@gmail.com>
2025-07-22 11:10:15 +0200

    update wordundercursor styles mini.nvim

--- nvim/lua/config/plugins/mini.lua
+++ nvim/lua/config/plugins/mini.lua
@@ -5,11 +5,28 @@ return {
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = false }
 
-      -- local cursorword = require 'mini.cursorword'
-      -- cursorword.setup { delay = 300 }
-      -- -- ✅ Set underline highlight for cursorword
+      local cursorword = require 'mini.cursorword'
+      cursorword.setup { delay = 0 }
+      -- ✅ Set underline highlight for cursorword
       -- vim.api.nvim_set_hl(0, 'MiniCursorword', { underline = true })
-      -- vim.api.nvim_set_hl(0, 'MiniCursorwordCurrent', { underline = true }) -- Optional: highlight current word differently
+      -- vim.api.nvim_set_hl(0, 'MiniCursorwordCurrent', { underline = false }) -- Optional: highlight current word differently
+      vim.api.nvim_set_hl(0, 'MiniCursorword', { bg = "#884499" })
+      vim.api.nvim_set_hl(0, 'MiniCursorwordCurrent', { underline = false }) -- Optional: highlight current word differently
+
+      -- TODO:
+      local hipatterns = require('mini.hipatterns')
+      hipatterns.setup({
+        highlighters = {
+          -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+          fixme     = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+          hack      = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+          todo      = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+          note      = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+
+          -- Highlight hex color strings (`#rrggbb`) using that color
+          hex_color = hipatterns.gen_highlighter.hex_color(),
+        },
+      })
     end
   }
 }
