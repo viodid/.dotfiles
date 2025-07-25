6c5821c04ad721b982edb43d805e043b7ad20c63
viodid <david.yunta.aller@gmail.com>
2025-07-25 14:23:46 +0200

    rm config

--- nvim/lua/config/plugins/cellular-automation.lua
+++ nvim/lua/config/plugins/cellular-automation.lua
@@ -3,10 +3,5 @@ return {
     'eandrju/cellular-automaton.nvim',
     enabled = true,
     lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'tokyonight-moon'
-    end,
-    opts = {},
   }
 }
