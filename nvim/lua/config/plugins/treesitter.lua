3ece31f93a5809afc5d1e87cd516b6d1f54a1219
viodid <david.yunta.aller@gmail.com>
2025-07-25 14:30:21 +0200

    add treesitter for cpp

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
     config = function()
       require 'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = { "c", "go", "lua", "python", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        ensure_installed = { "c", "cpp", "go", "lua", "python", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
