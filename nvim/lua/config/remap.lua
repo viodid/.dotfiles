793c2e4c0bb71200af4eda1372403a6ccc7ff40b
viodid <david.yunta.aller@gmail.com>
2026-01-03 14:34:36 +0100

    add diagnostic disable remap

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -53,3 +53,19 @@ vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
 -- diagnostics
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+local function hide_diagnostics()
+  vim.diagnostic.config({ -- https://neovim.io/doc/user/diagnostic.html
+    virtual_text = false,
+    signs = false,
+    underline = false,
+  })
+end
+local function show_diagnostics()
+  vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+  })
+end
+vim.keymap.set("n", "<leader>dh", hide_diagnostics)
+vim.keymap.set("n", "<leader>ds", show_diagnostics)
